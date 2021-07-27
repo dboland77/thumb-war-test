48ece9034f122244d63d4c132455e0cc970b9e91
Dave Boland <dboland77@gmail.com>
2021-07-27 14:25:02 +0100

    Monkey patching

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -3,10 +3,15 @@ const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
 const originalGetWinner = utils.getWinner
+
+// The line below mocks the getWinner function
 utils.getWinner = (p1, p2) => p1
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 
-// cleanup
+// cleanup - re-assign the getWinner to its original 
+// This method is called monkey patching - overwriting it and then making
+// it deterministic
+
 utils.getWinner = originalGetWinner
